8e54b5cd05ad026e0b53d54e038af354ceef76cb
Patrick Lloyd <patrick@lloyd.sh>
2019-04-17 02:56:37 -0700

    began implementing stat.h to check if file or directory. not done yet

--- devil/test.c
+++ devil/test.c
@@ -1,6 +1,7 @@
 #define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 
 #define DEFAULT_DIR "/mnt/iso/"
@@ -9,6 +10,7 @@ int main ( int argc, char *argv[] )
 {	
 	DIR *dp;
 	struct dirent *ep;
+	struct stat sb;
 	
 	dp = opendir (argc > 1 ? argv[1] : DEFAULT_DIR);
 	
@@ -16,11 +18,15 @@ int main ( int argc, char *argv[] )
 	{
 		while (ep = readdir (dp))
 			puts (ep->d_name);
-
+		/* if (stat(pathname, &sb) == 0 && S_ISDIR(sb.st_mode))
+		   {
+			...it is a directory...
+		   } */
 		(void) closedir (dp);
 	}
 	else
-		perror ("Couldn't open the directory");
+		perror ("Couldn't open the directory. Is...it...real?");
+		return EXIT_FAILURE;
 
 	return 0;
 }
